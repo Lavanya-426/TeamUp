37e081f65365a97d2f2db368513289d0fae5103c
Lavanya <lavanyaanna246@gmail.com>
2026-04-10 06:37:04 +0530

    added auth routes

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
