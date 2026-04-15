c4af09980729ec2eac44ff7d35454c1c829d117d
Lavanya <lavanyaanna246@gmail.com>
2026-04-15 16:47:00 +0530

    defined project identifier util logic{

---
+++ backend/utils/projectIdentificationUtils.js
@@ -0,0 +1,26 @@
+const isSameProject = (teamA, teamB) => {
+  if (teamA.type !== teamB.type) return false;
+
+  if (teamA.type === "COURSE") {
+    return (
+      teamA.course_id.toString() === teamB.course_id.toString() &&
+      teamA.slot === teamB.slot &&
+      teamA.teacher === teamB.teacher
+    );
+  }
+
+  if (teamA.type === "CAPSTONE") {
+    return (
+      teamA.school === teamB.school &&
+      teamA.specialization === teamB.specialization
+    );
+  }
+
+  if (teamA.type === "ECS") {
+    return true; // all ECS same category
+  }
+
+  return false;
+};
+
+export default isSameProject;
