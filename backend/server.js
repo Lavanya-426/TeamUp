80429b8e770aa00575adf39efec4b30887488f90
Lavanya <lavanyaanna246@gmail.com>
2026-04-19 16:41:46 +0530

    tested auth routes and a few others in request routes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(express.json());
 
 const mongoose = require("mongoose");
-
 mongoose
   .connect("mongodb://127.0.0.1:27017/teamup")
   .then(() => console.log("MongoDB connected"))
@@ -21,13 +20,13 @@ const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
 
 const requestRoutes = require("./routes/requestRoutes");
-app.use("/api/request", requestRoutes);
+app.use("/api/requests", requestRoutes);
 
 const teamRoutes = require("./routes/teamRoutes");
-app.use("/api/team", teamRoutes);
+app.use("/api/teams", teamRoutes);
 
 const userRoutes = require("./routes/userRoutes");
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 
 const discoveryRoutes = require("./routes/teamDiscoveryRoutes");
 app.use("/api/discover", discoveryRoutes);
@@ -35,3 +34,5 @@ app.use("/api/discover", discoveryRoutes);
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
+
+module.exports = app;
